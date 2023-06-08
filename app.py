99257f20efa59d2f5e2eec32375ea8d8a527e9c6
EdgarDr10 <136005642+EdgarDr10@users.noreply.github.com>
2023-06-08 17:49:28 +0000

    Test_1

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
-def main():
-    st.title("Hola worlds!")
-    st.write("This is a super cool Streamlit app.")
+st.title("Hola worlds!")
+st.write("This is a super cool Streamlit app.")
 
-if __name__ == '__main__':
-    main()
+
+import streamlit as st
+
+Valor = st.slider('Indica el valor ', 0, 10, 2)
+st.write("este es mi valor = ", Valor)
+st.write("este es mi valor + 10= ", Valor+10)
+
+if Valor >5:
+    print("mayor que 5")
+else:
+    print("menor que 5")
