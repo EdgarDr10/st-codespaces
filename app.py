53f67f8ca5c67e5e0aad753d62ae6b64d85997c4
EdgarDr10 <136005642+EdgarDr10@users.noreply.github.com>
2023-06-08 18:14:09 +0000

    test_5

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ st.write("This is a super cool Streamlit app.")
 
 import streamlit as st
 
-Valor = st.slider('Indica el valor ', 0, 10)
+Valor = st.sidebar.slider('Indica el valor ', 0, 10)
 st.write("este es mi valor = ", Valor)
 st.write("este es mi valor + 10= ", Valor+10)
 
 
-agree = st.checkbox('El valor es mayor que 5')
+agree = st.sidebar.checkbox('El valor es mayor que 5')
 
 if Valor >5 and agree:
     st.write("mayor que 5")
