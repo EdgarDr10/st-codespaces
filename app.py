34b879c6196e5098293919c124f70796b5bffb5c
EdgarDr10 <136005642+EdgarDr10@users.noreply.github.com>
2023-06-08 17:58:53 +0000

    test_2

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ Valor = st.slider('Indica el valor ', 0, 10, 2)
 st.write("este es mi valor = ", Valor)
 st.write("este es mi valor + 10= ", Valor+10)
 
-if Valor >5:
-    print("mayor que 5")
-else:
-    print("menor que 5")
+
+agree = st.checkbox('El valor es mayor que 5')
+
+if Valor >5 and agree:
+    st.write("mayor que 5")
+
